06d5c6c8ed2c3332c2616aa6c1e9197931e8aa39
postboy <anotherdiskmag@gmail.com>
2015-08-05 00:37:54 +0300

    better shell scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-#compilation & launch
+#just a compilation
 
 #compile with all warnings, using libgtk, in object file "ost"
 gcc -Wall `pkg-config --cflags gtk+-3.0` src/main.c src/crypto.c src/stuff.c src/tweetnacl.c `pkg-config --libs gtk+-3.0` -o ost
-#and launch
-./ost
